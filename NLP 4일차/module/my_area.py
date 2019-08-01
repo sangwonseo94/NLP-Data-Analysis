0ad089df12e198755cec6a1a49a26e806cbe8937
sangwonseo <gotkddnjs@naver.com>
2019-08-01 17:47:39 +0900

    2019-08-01

---
+++ NLP 4일차/module/my_area.py
@@ -0,0 +1,10 @@
+    #File name: my_area.py
+    
+PI =3.14
+    
+def square_area(a):
+    return a**2
+    
+def cricle_area(r):
+    return PI*(r**2)
+    
---
+++ NLP 4일차/module/my_module_test.py
@@ -0,0 +1,9 @@
+
+def fun(a):
+    print("입력된 숫자 : ",a)
+        
+if (__name__=='__main__'):
+    fun(3)
+    fun(4)
+else:
+    print("name이 import입니다.")
